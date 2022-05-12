8e47f484225e34e99781660977a97fb8e42f6b10
rallierod <99930994+rallierod@users.noreply.github.com>
2022-05-11 21:21:56 -0500

    printJob

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,8 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        someJobs = new ArrayList<>();
+         System.out.println(someJobs);
+//        System.out.println("printJobs is not implemented yet");
     }
 }
